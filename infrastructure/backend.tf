705a92e37d6530ad5efc93c576bf10e83efb79c7
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-03 15:44:07 +0000

    backend and precommit test

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,8 @@
 
 terraform {
   backend "azurerm" {
+
+    # My DToS dev env
     resource_group_name  = "rg-dtos-state"
     storage_account_name = "samacdtosdevstate"
     container_name       = "tfstate"
