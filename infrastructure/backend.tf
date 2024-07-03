b8f8c957337d1b7bf1117d301378708fff828607
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-07-03 15:31:29 +0000

    docker + backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,9 @@
 
 terraform {
   backend "azurerm" {
-
+    resource_group_name  = "rg-dtos-state"
+    storage_account_name = "samacdtosdevstate"
+    container_name       = "tfstate"
+    key                  = "dev.dtos.tfstate"
   }
 }
