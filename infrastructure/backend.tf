8f34e029312cbbdcbaf047a3a8d201f79581c31f
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-04 18:06:23 +0000

    comment backend again

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,10 +3,10 @@ terraform {
   backend "azurerm" {
 
     # My DToS dev env
-    resource_group_name  = var.STATE_RG
-    storage_account_name = var.STATE_SA
-    container_name       = var.STATE_CONT
-    key                  = var.STATE_BLOB
+    # resource_group_name  = var.STATE_RG
+    # storage_account_name = var.STATE_SA
+    # container_name       = var.STATE_CONT
+    # key                  = var.STATE_BLOB
     # resource_group_name  = "rg-dtos-state"
     # storage_account_name = "samacdtosdevstate"
     # container_name       = "tfstate"
