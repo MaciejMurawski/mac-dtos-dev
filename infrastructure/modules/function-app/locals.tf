eabc917bfeed829446c4946111d666326803b581
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-30 16:21:35 +0100

    locals

--- infrastructure/modules/function-app/locals.tf
+++ infrastructure/modules/function-app/locals.tf
@@ -13,8 +13,8 @@ locals {
       FUNCTIONS_WORKER_RUNTIME = "dotnet-isolated"
       AzureWebJobsStorage = "UseDevelopmentStorage=true" ## ??
       caasfolder_STORAGE = "UseDevelopmentStorage=true" ## ?? aws_instance.blue.*.id
-      targetFunction = "http://${azurerm_linux_function_app.function.[processCaasFile].defaul_hostname}/api/processCaasFile"
-      FileValidationURL = "http://${azurerm_linux_function_app.function.[FileValidation].defaul_hostname}/api/FileValidation" #"http://localhost:7073/api/FileValidation"
+      targetFunction = "http://${azurerm_linux_function_app.function.processCaasFile.defaul_hostname}/api/processCaasFile"
+      FileValidationURL = "http://${azurerm_linux_function_app.function.FileValidation.defaul_hostname}/api/FileValidation" #"http://localhost:7073/api/FileValidation"
     }
 
     ProcessCaasFile = {
