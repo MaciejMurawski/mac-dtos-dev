31461bca77ece79028616157735209effefef20a
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-30 13:24:42 +0100

    fnapps hostname outputs

---
+++ infrastructure/modules/function-app/outputs.tf
@@ -0,0 +1,5 @@
+
+output "function_app_hostname" {
+  description = "The default hostnames of the deployed Linux Function Apps."
+  value       = { for k, function in azurerm_linux_function_app.function : k => function.default_hostname }
+}
