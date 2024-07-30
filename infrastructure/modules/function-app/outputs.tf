98ef60e328ebdcb0ca9eefe65b209d8e22d08a0d
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-30 14:12:20 +0100

    composed url output

--- infrastructure/modules/function-app/outputs.tf
+++ infrastructure/modules/function-app/outputs.tf
@@ -6,6 +6,6 @@ output "function_app_hostname" {
 
 output "function_app_url" {
   description = "Composed URL of the deployed Linux Function Apps."
-  value       = { for k, function in azurerm_linux_function_app.function : k => "https://${function.default_hostname}/api/?code=${data.azurerm_function_app_host_keys.fnapphostkey[function.name]}" }
+  value       = { for k, function in azurerm_linux_function_app.function : k => "https://${function.default_hostname}/api/?code=${data.azurerm_function_app_host_keys.fnapphostkey[function.name_suffix]}" }
     #for k, function in azurerm_linux_function_app.function : k => "https://${function.default_hostname}/api/${local.function_foo}?code=${data.azurerm_function_app_host_keys.fnapphostkey.default_function_key}"
 }
