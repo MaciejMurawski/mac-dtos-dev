2c40ac80b2f361778cd1a7b6d3a07e94eaab063c
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-30 16:31:50 +0100

    locals

--- infrastructure/modules/function-app/locals.tf
+++ infrastructure/modules/function-app/locals.tf
@@ -13,8 +13,8 @@ locals {
       FUNCTIONS_WORKER_RUNTIME = "dotnet-isolated"
       AzureWebJobsStorage = "UseDevelopmentStorage=true" ## ??
       caasfolder_STORAGE = "UseDevelopmentStorage=true" ## ?? aws_instance.blue.*.id
-      targetFunction    = "http://${var.names.function-app}-${lower(var.function_app.fa_config.receiveCaasFile.name_suffix)}/api/processCaasFile"
-      FileValidationURL = "http://${var.names.function-app}-${lower(var.function_app.fa_config.FileValidation.name_suffix)}/api/FileValidation"
+      targetFunction    = "http://${var.names.function-app}-${lower(var.function_app.receiveCaasFile.name_suffix)}/api/processCaasFile"
+      FileValidationURL = "http://${var.names.function-app}-${lower(var.function_app.FileValidation.name_suffix)}/api/FileValidation"
     }
 
     ProcessCaasFile = {
