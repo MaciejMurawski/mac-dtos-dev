4eebe5a11db18e35b71a4fc105dfab02debac128
Maciej Murawski <maciej.murawski@outlook.com>
2024-07-31 14:55:22 +0100

    fnapps docker settings - mi config

--- infrastructure/modules/azure-container-registry/output.tf
+++ infrastructure/modules/azure-container-registry/output.tf
@@ -3,10 +3,6 @@ output "login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
-output "mi_client_id" {
-  value = azurerm_user_assigned_identity.uai.client_id
-}
-
 output "mi_id" {
   value = azurerm_user_assigned_identity.uai.id
 }
