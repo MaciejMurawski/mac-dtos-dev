dd5fc0ce7bc04c7fb5da8af3f44e426df8bfc568
Maciej Murawski <maciej.murawski@outlook.com>
2024-08-21 13:34:21 +0100

    comment output

--- infrastructure/modules/log-analytics-workspace/output.tf
+++ infrastructure/modules/log-analytics-workspace/output.tf
@@ -6,10 +6,10 @@ output "name" {
   value = azurerm_log_analytics_workspace.law.name
 }
 
-output "audit_id" {
-  value = azurerm_log_analytics_workspace.law_audit.id
-}
+# output "audit_id" {
+#   value = azurerm_log_analytics_workspace.law_audit.id
+# }
 
-output "audit_name" {
-  value = azurerm_log_analytics_workspace.law_audit.name
-}
+# output "audit_name" {
+#   value = azurerm_log_analytics_workspace.law_audit.name
+# }
