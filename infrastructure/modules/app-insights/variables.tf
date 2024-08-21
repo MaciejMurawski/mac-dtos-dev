81a1da2ea2ab5d560d92cf6581eb6396e6f0aea6
Maciej Murawski <maciej.murawski@outlook.com>
2024-08-21 13:32:22 +0100

    fix with audit_law_id

--- infrastructure/modules/app-insights/variables.tf
+++ infrastructure/modules/app-insights/variables.tf
@@ -35,7 +35,7 @@ variable "law_id" {
 
 ### DevOps subsripion AppInsights
 
-variable "audit_law_id" {
-  type        = string
-  description = "Is the LAW workspace ID in Audit subscription."
-}
+# variable "audit_law_id" {
+#   type        = string
+#   description = "Is the LAW workspace ID in Audit subscription."
+# }
